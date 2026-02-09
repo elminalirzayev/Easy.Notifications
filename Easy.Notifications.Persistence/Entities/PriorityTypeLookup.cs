9b9194c0527f2eba6d2b3c87a293c0c8a74b6292
Elmin Alirzayev <elmin.alirzayev@gmail.com>
2026-02-09 05:46:15 +0300

    document format

--- Easy.Notifications.Persistence/Entities/PriorityTypeLookup.cs
+++ Easy.Notifications.Persistence/Entities/PriorityTypeLookup.cs
@@ -4,7 +4,6 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Easy.Notifications.Persistence.EntityFramework.Entities
 {
-    
     [Table("PriorityTypes")]
     public class PriorityTypeLookup
     {
